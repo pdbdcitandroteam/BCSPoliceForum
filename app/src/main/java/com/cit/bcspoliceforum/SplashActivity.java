a38fb28e2a3aedac0eca345897c1a192cff2f241
tshuvo <tshuvo6@gmail.com>
2016-01-17 14:14:57 +0600

    Database folder and DbHelper 1

--- app/src/main/java/com/cit/bcspoliceforum/SplashActivity.java
+++ app/src/main/java/com/cit/bcspoliceforum/SplashActivity.java
@@ -3,13 +3,6 @@ package com.cit.bcspoliceforum;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-
-import com.cit.bcspoliceforum.fragment.FragDrawer;
 
 public class SplashActivity extends Activity {
 
